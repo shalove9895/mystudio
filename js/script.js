65510b93e51c08326a8376734790ed04470508a0
shalove9895 <shalovearts0@gmail.com>
2019-07-08 14:54:12 +0530

    adding

--- js/script.js
+++ js/script.js
@@ -16,3 +16,47 @@ $(function() {
     }
   });
 });
+
+
+var Rotation = (function () {
+  var p = Rotation.prototype;
+
+  function Rotation($elem) {
+    this._$elem = $elem;
+    this._$child = $elem.children();
+    this._props = {};
+    
+    this._onResize();
+    this._handleEvents();
+  }
+
+  p._handleEvents = function () {
+    this._$elem.on('mousemove', this._onMousemove.bind(this));
+    this._$elem.on('mouseout', this._onMouseout.bind(this));
+  }
+  
+  p._onMousemove = function (e) {
+    var _x = e.offsetX - this._props.center[0];
+    var _y = e.offsetY - this._props.center[1];
+    
+    TweenMax.to(this._$child, .1, {
+      rotationX: _y / 30,
+      rotationY: _x / 30
+    });
+  };
+  
+  p._onMouseout = function () {
+    TweenMax.to(this._$child, .3, {
+      rotationX: 0,
+      rotationY: 0
+    });
+  };
+  
+  p._onResize = function () {
+    this._props.center = [this._$elem.width() / 2, this._$elem.height() / 2];
+  };
+  
+  return Rotation;
+}());
+
+new Rotation($('.js-rotation'));
